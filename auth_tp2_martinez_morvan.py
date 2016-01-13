ef1c448de8db9e0b6c55fdf78c13ef76652b3a67
macwindou <thibaut.001@hotmail.fr>
2016-01-13 10:04:14 +0100

    finition du generehash + commencement de message, manque chiffrement AES

--- auth_tp2_martinez_morvan.py
+++ auth_tp2_martinez_morvan.py
@@ -1,42 +1,55 @@
 import random
 import hashlib
+from Crypto.Cipher import AES
+
+#benjamin.arnaud.72@gmail.com
 
 def hash(pre_puzzle_key, n):
-	i=0
-	m = hashlib.md5()
+	m = hashlib.md5(pre_puzzle_key).digest()
 
-	while i < 1000:
+	for i in range(1, n):
+		m = hashlib.md5(m).digest()
 
-		m.update(bytearray(pre_puzzle_key))
-		m.digest()
-		i=i+1
-	#if n > 1:
-	#	print(m.digest(),n)
-	#	hash(m.digest(), n-1)
-	#else:
-	#	return m.digest()
+	return m
 
 def generate_paires(n):
 	paires=[]
 	for i in range(0, n):
-		pre_puzzle_key = random.getrandbits(128)
-		secret_key = random.getrandbits(128)
+		pre_puzzle_key = bytearray(str(random.getrandbits(128)), "ascii")
+		print(type(pre_puzzle_key))
+		secret_key = bytearray(str(random.getrandbits(128)), "ascii")
 		paires.append([pre_puzzle_key, secret_key])
 
 	return paires
 
 def generate_puzzle(list):
 	puzzle_key = []
+	secret_key= []
 	pre_puzzle_key = [(pre_puzzle_key) for (pre_puzzle_key, secret_key) in list]
 	secret_key = [(secret_key) for (pre_puzzle_key, secret_key) in list]
+	print("secret key generate puzzle ")
+	print(secret_key)
 
 	for i in range(0, len(pre_puzzle_key)):
 		print (i,"->",pre_puzzle_key[i])
-		puzzle_key[i] = hash(pre_puzzle_key[i], 1000)
+		puzzle_key.insert(i, hash(pre_puzzle_key[i], 1000))
 
 
-	print(pre_puzzle_key)
+
+	print("puzzle key :")
+	print(puzzle_key)
+	print("secret key : ")
 	print(secret_key)
+
+	message = []
+	for i in range(0, len(list)):
+		secret_key[i].append(i)
+		message.append(secret_key[i])
+		print("message : ")
+		print(message)
+
+	obj = AES.new(secret_key[i], AES.MODE_CBC)
+	ciphertext = obj.encrypt(message)
 
 def User_puzzle ():
 	liste = [(pre_puzzle_key, secret_key) for (pre_puzzle_key, secret_key) in generate_paires(3)]
